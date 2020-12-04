b5e2a94a16bc9f2612f6093577a9a5a73b4d1503
Catalin Boiangiu <work@catalinboiangiu.ro>
2020-12-04 22:31:32 +0200

    fix printStatus method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/local/bin/node
 
-console.log("create-fullstack-app");
+console.log("create-fullstack-app\n");
 
 var exec = require('child_process').execSync;
 var chalk = require('chalk');
@@ -8,11 +8,11 @@ var yargs = require("yargs").argv;
 var path = require('path');
 var fs = require('fs');
 
-const printStatus = (parent, status, separator) => {
-    let line = "________________________________";
-    console.log(chalk.grey(line.replace("_", separator)));
-    console.log(chalk.grey('${parent}: ${status}'));
-    console.log(chalk.grey(line.replace("_", separator)));
+const printStatus = (parent, status, separator = "|") => {
+    const line = "________________________________".replace(/_/g, separator);
+    console.log(chalk.grey(line));
+    console.log(chalk.grey(`${parent}: ${status}`));
+    console.log(chalk.grey(line));
 };
 
 printStatus("Setup", "started");
